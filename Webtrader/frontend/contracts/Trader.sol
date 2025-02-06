fb625f23566d7c9ec79eeca566f1d274aa70fd59
Shuga8 <lotocharles8@gmail.com>
2025-02-06 22:16:02 +0100

    Structure setup

---
+++ Webtrader/frontend/contracts/Trader.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.28;
+
+import "hardhat/console.sol";
+
+contract Trader{
+    address  ethAddress;
+
+    constructor(){
+        ethAddress = 0xc0b751d98f041Db3f2854DFa911E6e5fb3d39A3e;
+    }
+}
